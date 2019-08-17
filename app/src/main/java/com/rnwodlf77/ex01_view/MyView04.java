7b1c0978cc7045264e9abf256e4f776a44aad72b
tkillman <dkkim1004@hanmail.net>
2019-08-17 18:30:00 +0900

    haha

---
+++ app/src/main/java/com/rnwodlf77/ex01_view/MyView04.java
@@ -0,0 +1,14 @@
+package com.rnwodlf77.ex01_view;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyView04 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myview04);
+    }
+}
